55927197874bf3324e2791e07fe3805bc1797bd4
Clayton Moura <clee.gomes@gmail.com>
2024-05-20 20:50:52 -0300

    Desafio concluído.

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,36 @@
+
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Teste");
+        //TODO: IMPORTAR CLASSE SCANNER - done
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        // importar Ramdom para gerar numeros aleatórios - done
+        Random random = new Random();
+
+        // EXIBIR MENSAGENS PARA O NOSSO USUÁRIO - done
+        //OBTER PELO SCANNER OS VALORES DIGITADOS NO TERMINAL - done
+        //EXIBIR A MENSAGEM CONTA CRIADA
+        
+        int numeroConta;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        System.out.println("Por favor, digite seu nome: ");
+        nomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite sua Agência [ 123-1 ]: ");
+        agencia = scanner.next();
+
+        System.out.println("Quanto gostaria de depositar para abertura de conta? ");
+        saldo = scanner.nextDouble();
+
+        numeroConta = random.nextInt();
+
+        System.out.println("Olá, " + nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + ", conta " + numeroConta + ", e seu saldo R$ " + saldo + " já está disponível para saque.");
     }
 }
